67e1db29df5dfe17529030c21306a9fe238010ed
smiley405 <smileypiratesgame@gmail.com>
2025-10-22 21:52:41 +0100

    fix: add alice falling logics

--- src/entities/alice/alice.gd
+++ src/entities/alice/alice.gd
@@ -1,5 +1,5 @@
 class_name Alice
-extends Node
+extends Node2D
 
 
 var _loved_cat: bool = false
@@ -9,6 +9,21 @@ var _loved_cat: bool = false
 
 func _ready() -> void:
 	_animated_sprite.play("wave")
+
+
+func _do_fall() -> void:
+	_animated_sprite.play("fall")
+	var _tween = create_tween()
+	var target_y = position.y + 96.0
+	_tween.tween_property(self, "position:y", target_y, 1.0).set_delay(0.2)
+
+
+## This function is called internally by the tools/trigger [br]
+## [param entity] - is a node which trrigered it. i.e player, enemies [br]
+## [param trigger] - is the node that's been triggered
+func triggered_by(from: Node2D, trigger: Node2D) -> void:
+	if from.is_in_group("player"):
+		_do_fall()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
--- src/entities/falling_tiles/falling_tiles.gd
+++ src/entities/falling_tiles/falling_tiles.gd
@@ -10,12 +10,24 @@ var _collapsed: bool = false
 func _ready() -> void:
 	_animated_sprite.stop()
 
+
+func _collapse() -> void:
+	_collapsed = true
+	_animated_sprite.play("fall")
+	Events.camera_shake.emit()
+
+
+## This function is called internally by the tools/trigger [br]
+## [param entity] - is a node which trrigered it. i.e player, enemies [br]
+## [param trigger] - is the node that's been triggered
+func triggered_by(from: Node2D, trigger: Node2D) -> void:
+	if from.is_in_group("player"):
+		_collapse()
+
+
 # Detects player > body:collisionShape2d, to detect area2d use area-entered or leeave
 # The signal detector node should have the mask pointing to palyer, not the other way round
 func _on_body_entered(body: Node2D) -> void:
 	if _collapsed or not body.is_in_group("player"):
 		return
-
-	_collapsed = true
-	_animated_sprite.play("fall")
-	Events.camera_shake.emit()
+	_collapse()
