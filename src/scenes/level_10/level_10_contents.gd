3f49c888114875c252da33e27ea34d1bc3bc1287
smiley405 <smileypiratesgame@gmail.com>
2025-11-03 20:43:35 +0000

    fix(level_10): set_scene to 0 on pressing x key

--- src/scenes/level_10/level_10_contents.gd
+++ src/scenes/level_10/level_10_contents.gd
@@ -9,5 +9,11 @@ func _ready() -> void:
 	Events.game_finished.connect(_on_game_finished)
 
 
+func _process(delta: float) -> void:
+	if GameState.game_won:
+		if Input.is_action_just_pressed("attack"):
+			SceneManager.set_scene(0)
+
+
 func _on_game_finished() -> void:
 	_animation_player.play("end_credits")
